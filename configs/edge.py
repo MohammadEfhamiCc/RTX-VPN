80091f46850eaabea5f74c5057e15034170dfa54
Sir.MmD <27060102+Sir-MmD@users.noreply.github.com>
2025-02-28 00:46:00 +0330

    Update edge.py

--- configs/edge.py
+++ configs/edge.py
@@ -20,6 +20,15 @@ def run_command(command):
     except subprocess.CalledProcessError as e:
         log(f"Command failed: {command} - {e}")
 
+def start_process(command, name):
+    try:
+        process = subprocess.Popen(command, shell=True)
+        log(f"{name} started with PID {process.pid}")
+        return process
+    except Exception as e:
+        log(f"Error starting {name}: {e}")
+        sys.exit(1)
+        
 rathole_process = start_process("/opt/rtxvpn_v2/edge/rathole /opt/rtxvpn_v2/edge/edge.toml", "Rathole")
 xray_process = start_process("/opt/rtxvpn_v2/edge/xray run -c /opt/rtxvpn_v2/edge/edge.json", "Xray")
 
@@ -28,7 +37,7 @@ log("VPN Service Started")
 def shutdown_handler(signum, frame):
     """Handle shutdown signals."""
     log("VPN Service interrupted")
-    for process in [rathole_process, tun2socks_process, xray_process]:
+    for process in [rathole_process, xray_process]:
         if process:
             process.terminate()
     sys.exit(1)
